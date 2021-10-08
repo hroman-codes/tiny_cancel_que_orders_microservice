54f4896040da0c80d8420545446ef134c79be0b4
getromandev <getroman.dev@gmail.com>
2021-10-08 11:24:08 -0400

    add functionality to get endpoint that will instantiate a recharge session and create a subscription/cancelled webhook

--- server.js
+++ server.js
@@ -1,56 +1,47 @@
-const { response } = require('express')
 const express = require('express')
-const bodyParser = require('body-parser')
-
 const app = express()
+
+const { response } = require('express')
+const Axios = require('axios');
+
 const port = 8080
 const host = '0.0.0.0'
 require('dotenv').config()
 
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
 
 app.get('/', function (req, res) {
-    res.send('GET')
+    // instantiate a recharge session (include the api token)
+    const rechargeClient = Axios.create({
+        baseURL: 'https://api.rechargeapps.com/',
+        timeout: 29000,
+        headers: {
+          'Accept': 'application/json; charset=utf-8;',
+          'Content-Type': 'application/json',
+          'X-Recharge-Access-Token': process.env.RECHARGE_TEST_API_KEY,
+          'secrete': process.env.APP_SECRETE,
+        }
+      });
+
+    // // create a webhook
+    rechargeClient.post('https://api.rechargeapps.com/webhooks', {
+            "address": "https://a035-173-77-234-181.ngrok.io/subscription/cancelled",
+            "topic": "subscription/cancelled"
+        }
+    )
+    .then((promiseRes) => {
+        res.send(promiseRes.data)
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 })
 
-app.post('/subscription/cancelled', function (req, res) {
-    const api_key = process.env.RECHARGE_TEST_API_KEY;
-    const webHookURL = 'https://api.rechargeapps.com/webhooks'
-    
-    console.log(req.body);
- 
-    // set up the webhook
-    // send a request 
-    // log out the response
-
+app.post('/subscription/webhook', function (req, res) {
     res.send('POST')
     res.status(200).end()
 })
-
-// app.post('/subscription/cancelled', function (req, res) {
-//     const api_key = process.env.RECHARGE_TEST_API_KEY;
-//     let headers = {
-//         "Content-Type": "application/json",
-//         "X-Recharge-Access-Token": "your_api_token"
-//     }
-//     let url = "https://api.rechargeapps.com/webhooks";
-//     let data = {
-//         "address": "https://request.in/foo",
-//         "topic": "subscription/cancelled"
-//     }
-
-//     res.send('POST')
-// })
-
-// app.post('https://1465-173-77-234-181.ngrok.io/subscriptions/:id/cancel', function (req, res) {
-//     const api_key = process.env.RECHARGE_TEST_API_KEY;
-//     const tempID = '27363808'
-
-//     console.log('req', req)
-//     console.log('res', res)
-
-//     res.send('TEST POST WITH NGROK >>> Recharge WEBHOOK Working')
-// })
 
 app.listen(port, host);
 
