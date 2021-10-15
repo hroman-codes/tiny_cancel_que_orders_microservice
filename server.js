e216a1365ebf99f0b6ff4ad9133db9506691c9b5
getromandev <getroman.dev@gmail.com>
2021-10-15 00:00:46 -0400

    add logic to delete queued orders if the customer does not have any other active subscriptions

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/subscription/webhook', urlencodedParser, function (req, res) {
         if (activeSubscription.length === 0) {
             // proceed to check for queued orders ✅
             console.log('Customer doesnt have active subs go ahead check for queued orders ✅')
-            // `https://73cd-173-77-234-181.ngrok.io/orders` <<< ake api json server
+            // `https://73cd-173-77-234-181.ngrok.io/orders` <<< fake api json server
             // `https://api.rechargeapps.com/orders?customer_id=${customerID}&status=QUEUED` <<< Real recharge end point
             axios.get(`https://73cd-173-77-234-181.ngrok.io/orders`, {
                     headers: {
@@ -89,9 +89,17 @@ app.post('/subscription/webhook', urlencodedParser, function (req, res) {
                     if (queuedOrders.length > 0) {
                         // iterate through the list of orders
                         for (let i = 0; i < queuedOrders.length; i++) {
-                            // delete the order 
-                            // customerOrders[i].status === 'QUEUED'
-                            console.log(`delete this order ${queuedOrders[i]} ❌`)
+                            let orderID = queuedOrders[i].id;
+                            console.log('orderID >>', orderID);
+                            console.log(`delete this order ${queuedOrders[i]} ❌`);
+                            // `https://api.rechargeapps.com/orders/${orderID}` << Real recharge end point
+                            // `https://73cd-173-77-234-181.ngrok.io/orders/${orderID}` << fake api json server
+                            // delete the order
+                            axios.delete(`https://73cd-173-77-234-181.ngrok.io/orders/${orderID}`, {
+                                headers: {
+                                    'X-Recharge-Access-Token': process.env.RECHARGE_TEST_API_KEY,
+                                    }
+                            })
                         }
                     } else {
                         // if no queued orders exist then do nothing / return out this promise
